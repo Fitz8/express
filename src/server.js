aa9995a6417ff9b337dd8b844adaef0e771af064
Fitz8 <liamfitzgerald@hotmail.co.uk>
2022-08-12 16:07:37 +0100

    added data limits

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const cors = require("cors");
 const userRouter = require("./user/routes");
 const app = express();
 
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
+  
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    parameterLimit: 100000,
+    extended: true 
+}));
+
 //add routes and controllers to app before listen runs
 app.use(express.json()); //Tell entire sever that it will always receive json and to always send back json
 app.use(cors());
