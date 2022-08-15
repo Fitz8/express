c3fe98bdf6afdea944828ed5ede83c3b173056a5
Fitz8 <liamfitzgerald@hotmail.co.uk>
2022-08-15 13:49:28 +0100

    Setting up for deployment

--- src/server.js
+++ src/server.js
@@ -4,24 +4,24 @@ const cors = require("cors");
 const userRouter = require("./user/routes");
 const app = express();
 
-app.use(bodyParser.json({
-    limit: '50mb'
-}));
+// app.use(bodyParser.json({
+//     limit: '50mb'
+// }));
   
-app.use(bodyParser.urlencoded({
-    limit: '50mb',
-    parameterLimit: 100000,
-    extended: true 
-}));
+// app.use(bodyParser.urlencoded({
+//     limit: '50mb',
+//     parameterLimit: 100000,
+//     extended: true 
+// }));
+
+app.get("/health", (req, res) => {
+    res.status(200).send({ message: "API is working" });
+});
 
 //add routes and controllers to app before listen runs
 app.use(express.json()); //Tell entire sever that it will always receive json and to always send back json
 app.use(cors());
 app.use(userRouter);
-
-app.get("/health", (req, res) => {
-    res.status(200).send({ message: "API is working" });
-});
 
 app.listen(5001, () => {
     console.log("Listening on port 5001");
